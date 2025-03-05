44e8a93e0fdf6ca4a3b6b15f5e8ecca127a0e9ab
Talha <talhasaleem@hashlogics.com>
2025-03-05 12:04:24 +0500

    add git ignore file

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const port = process.env.PORT || 5000;
+
+// Use morgan to log requests
+app.use(morgan('combined'));  // You can use 'tiny', 'dev', or 'combined' format
 
 // Define a simple route
 app.get('/', (req, res) => {
